c54573e1f8e3f06c60952ac51173b8149aebcb58
Puttipong <202909260+voaiutersna@users.noreply.github.com>
2025-12-12 06:21:54 +0700

    LAB02-update-exercise

---
+++ LAB-KMITL/excerise/EXCERCISE02-01-InfixToPostFixV01.py
@@ -0,0 +1,76 @@
+class ArrayStack:
+    def __init__(self):
+        self.size = 0
+        self.data = []
+    def push(self, input_data):
+        try:
+            if input_data.isdigit():
+                input_data = int(input_data)
+            elif input_data.isdigit(".","",1):
+                input_data = float(input_data)
+        except(TypeError, ValueError, ArithmeticError, AttributeError):
+            return None
+        finally:
+            self.data.append(input_data)
+            self.size += 1
+    def is_empty(self):
+        if not self.size:
+            return True
+        return False
+    def pop(self):
+        if self.is_empty():
+            print("Underflow: Cannot pop data from an empty list")
+            return None
+        self.size -= 1
+        return self.data.pop()
+    def get_stack_top(self):
+        if self.is_empty():
+            print("Underflow: Cannot get stack top from an empty list")
+            return None
+        return self.data[-1]
+    def get_size(self):
+        return self.size
+    def print_stack(self):
+        print(self.data)
+    
+def infixToPostfix(expression):
+    stack = ArrayStack()
+    operation = ["*","/","+","-"]
+    high_oper = ["*","/"]
+    low_oper = ["+","-"]
+    postfix = ""
+    for i in expression:
+        #print(i)
+        if i in operation:
+            if i == "*" or i =="/":
+                if stack.is_empty():
+                    stack.push(i)
+                else: 
+                    while True:
+                        top = stack.get_stack_top() 
+                        print("top working",top) 
+                        if top in high_oper:
+                            pop_oper = stack.pop() 
+                            postfix += pop_oper 
+                        if top in low_oper:
+                            stack.push(i)
+                            break
+            else:
+                if stack.is_empty():
+                    stack.push(i)
+                else:
+                    while not stack.is_empty():
+                        pop_item = stack.pop()
+                        postfix += pop_item
+                    stack.push(i)
+        else:
+            postfix += i
+    for i in range(len(stack.data)-1,-1,-1):
+        postfix += stack.data[i]
+    return postfix
+
+def main():
+    inp = input()
+    result = infixToPostfix(inp)
+    print(result)
+main()
---
+++ LAB-KMITL/excerise/EXCERCISE02-03-Spam.py
@@ -0,0 +1,66 @@
+class ArrayStack:
+    def __init__(self):
+        self.size = 0
+        self.data = []
+    def push(self, input_data):
+        try:
+            if input_data.isdigit():
+                input_data = int(input_data)
+            elif input_data.isdigit(".","",1):
+                input_data = float(input_data)
+        except(TypeError, ValueError, ArithmeticError, AttributeError):
+            return None
+        finally:
+            self.data.append(input_data)
+            self.size += 1
+    def is_empty(self):
+        if not self.size:
+            return True
+        return False
+    def pop(self):
+        if self.is_empty():
+            print("Underflow: Cannot pop data from an empty list")
+            return None
+        self.size -= 1
+        return self.data.pop()
+    def get_stack_top(self):
+        if self.is_empty():
+            print("Underflow: Cannot get stack top from an empty list")
+            return None
+        return self.data[-1]
+    def get_size(self):
+        return self.size
+    def print_stack(self):
+        print(self.data)
+
+def  spam(expression):
+    stack = ArrayStack()
+    open_bucket = ["(","{","["]
+    close_bucket = [")","}","]"]
+    for i in expression:
+        if i in open_bucket:
+            stack.push(i)
+            continue
+        if i in close_bucket:
+            top = stack.get_stack_top()
+            close = ""
+            if top == "(":
+                close = ")"
+            elif top == "{":
+                close = "}"
+            elif top == "[":
+                close = "]"
+            if close != i:
+                return False
+            elif close == i:
+                stack.pop()
+    if stack.is_empty():
+        return True
+    return False
+
+        
+def main():
+    inp = input()
+    result = spam(inp)
+    print(result)
+main()
